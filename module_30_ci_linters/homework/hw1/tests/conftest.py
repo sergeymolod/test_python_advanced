f0078f3d8c9af56d8d07fb1924906f20fdf51174
sergeymolod <molodavkin.s@yandex.ru>
2025-08-29 20:52:15 +0300

    Update conftest.py

--- module_30_ci_linters/homework/hw1/tests/conftest.py
+++ module_30_ci_linters/homework/hw1/tests/conftest.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 import pytest
 from main.app import create_app
-from main.models import Client, ClientParking, Parking
+from main.models import Client, Parking
 from main.models import db as _db
 
 
