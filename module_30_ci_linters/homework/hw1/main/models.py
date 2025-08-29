f70be62184f315d34848d17626fd1659d70cdbe1
sergeymolod <molodavkin.s@yandex.ru>
2025-08-29 21:08:17 +0300

    Update models.py

--- module_30_ci_linters/homework/hw1/main/models.py
+++ module_30_ci_linters/homework/hw1/main/models.py
@@ -2,7 +2,8 @@ import datetime
 from typing import Any, Dict, List
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey, String, UniqueConstraint
-from sqlalchemy.orm import (Mapped, declarative_base, mapped_column, relationship)
+from sqlalchemy.orm import (Mapped, declarative_base, mapped_column, 
+                            relationship)
 
 
 class Base(declarative_base):
