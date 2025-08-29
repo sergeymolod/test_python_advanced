dc5681546cfe42c83609bed7d38628d269315821
sergeymolod <molodavkin.s@yandex.ru>
2025-08-29 20:26:11 +0300

    Update app.py

--- module_30_ci_linters/homework/hw1/main/app.py
+++ module_30_ci_linters/homework/hw1/main/app.py
@@ -3,6 +3,8 @@ import sqlalchemy
 from flask import Flask, request
 from sqlalchemy import select, insert, update, and_
 
+
+
 def create_app(test_config=None):
     if test_config is None:
         db_name = 'sqlite:///parking.db'
