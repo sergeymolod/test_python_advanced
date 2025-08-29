88e3a6d46f7b42cb4e5237cb2745f0530318de47
sergeymolod <molodavkin.s@yandex.ru>
2025-08-29 20:22:34 +0300

    Update app.py

--- module_30_ci_linters/homework/hw1/main/app.py
+++ module_30_ci_linters/homework/hw1/main/app.py
@@ -1,10 +1,7 @@
 import datetime
-
 import sqlalchemy
 from flask import Flask, request
 from sqlalchemy import select, insert, update, and_
-# from faker import Faker
-
 
 def create_app(test_config=None):
     if test_config is None:
